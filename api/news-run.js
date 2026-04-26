08215fda6a038e0411ce15e362f995615ed1e54d
Hoa Duc Viet <hoaducviet@Viet-MBP.local>
2026-04-26 08:33:17 +0700

    Change format send notify news

--- api/news-run.js
+++ api/news-run.js
@@ -87,7 +87,7 @@ module.exports = async (req, res) => {
     // notify song song luôn
     await Promise.all(
       results
-        .filter((item) => !item.error)
+        .filter((item) => item.error)
         .map((item) =>
           sendNotify({
             type: "post-sites",
